71c8280dd6aa5eadf9418b4c0087ddb5a5e03053
Hussein The App <hus.sc@aol.com>
2020-10-15 22:18:04 +0300

    Initial Commit

---
+++ Jordan Developers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jordan Developers
+//
+//  Created by Hussein The Area on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jordan Developers/Jordan_DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jordan_DevelopersApp.swift
+//  Jordan Developers
+//
+//  Created by Hussein The Area on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct Jordan_DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
